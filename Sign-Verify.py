382e3046abca3125fcb959ebf89efe12c5d19a5e
IsmailiGresa <gressa.issmaili@gmail.com>
2022-12-29 12:32:29 +0100

    Create Sign-Verify.py

---
+++ Sign-Verify.py
@@ -0,0 +1,23 @@
+import sys
+
+def createUser(name):
+    print("User " + name + " has been created!")
+    f = open("C:\Users\IFES Yoga\Documents\GitHub\Sign-Verify\keys"+name+".xml", "w")
+    # if(f.__exit__):
+    #     print("Files exist")
+
+
+
+def writeMessage(message, sender, reciever):
+    print("Message was saved in file")
+
+def readMessage(message, receiver, sender):
+    print("Veq diqka me dal")
+
+if(sys.argv[1] == "create-user"):
+    createUser(sys.argv[2])
+elif(sys.argv[1] == "write-message"):
+    writeMessage(sys.argv[2], sys.argv[3], sys.argv[4])
+elif(sys.argv[1] == "read-message"):
+    readMessage(sys.argv[2], sys.argv[3], sys.argv[4])
+
