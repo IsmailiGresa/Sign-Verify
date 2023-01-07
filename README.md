9435355e2941d1760ae3e0ee236ab2e496cf4f1e
IsmailiGresa <gressa.issmaili@gmail.com>
2023-01-07 18:54:18 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Sign-Verify
+
+Commands on cmd:
+
+1.Create two users so the communication is set up:
+
+	python Sign-Verify.py create-user grese
+
+	python Sign-Verify.py create-user someone
+
+Users will be saved in the main directory and the private and public key get placed in keys
+
+2.Write the message from a user to another:
+
+	python Sign-Verify.py write-message 423857 grese someone
+
+The message is sent from grese to someone and is saved on the main directory as someone.json file
+With the message, the signature is also saved in the same file.
+
+3. Read the message the sender sent to the receiver:
+
+	python Sign-Verify.py read-message 423857 someone grese
+
+Displays the decrypted message and verifies the signature in the file.
+
+
+
+
--- Sign-Verify.py
+++ Sign-Verify.py
@@ -4,34 +4,34 @@ import math
 from pathlib import Path
 import re
 
-def generate_prime():	
+def generatePrime():	
 	p = random.randint(100, 1000) #Generating a random number between 100 and 1000
-	while not is_prime(p): #Checking if the number is prime or not
+	while not isPrime(p): #Checking if the number is prime or not
 		p = random.randint(100, 1000) #Generating again if it is not prime
 	return p
 
-def is_prime(n): #Defining function to check if a number is prime or not
+def isPrime(n): #Defining function to check if a number is prime or not
 	for i in range(2, int(math.sqrt(n)) + 1): #Looping over numbers from 2 to the square root of n
 		if n % i == 0: #Returning false if n is divisible by i
 			return False
 	return True #Returning true if n is not divisible by any number
 
 def generatePubKey(): #Defining function to generate private and public keys
-    p = generate_prime() #Generating two prime numbers
-    q = generate_prime()
+    p = generatePrime() #Generating two prime numbers
+    q = generatePrime()
     n = p * q #Calculating n and phi
     phi = (p - 1) * (q - 1)
     e = random.randint(1, phi)#Generating a random number between 1 and phi
     while math.gcd(e, phi) != 1:
-        e = random.randint(1, phi) #Generating again if they are not coprime
+        e = random.randint(2, phi) #Generating again if they are not coprime
     for d in range(1, phi): #Calculating d such that (d * e) % phi = 1
     	if (d * e) % phi == 1:
             break
-    return e, n #Returning the public and private keys
+    return (e, n) #Returning the public and private keys
 
 def generatePrivKey(): #Defining function to generate private and public keys
-    p = generate_prime() #Generating two prime numbers
-    q = generate_prime()
+    p = generatePrime() #Generating two prime numbers
+    q = generatePrime()
     n = p * q #Calculating n and phi
     phi = (p - 1) * (q - 1)
     e = random.randint(1, phi)#Generating a random number between 1 and phi
@@ -40,7 +40,7 @@ def generatePrivKey(): #Defining function to generate private and public keys
     for d in range(1, phi): #Calculating d such that (d * e) % phi = 1
     	if (d * e) % phi == 1:
             break
-    return d, n #Returning the public and private keys
+    return (d, n) #Returning the public and private keys
 
 def createUser(name):
 	print("User " + name + " has been created!")
@@ -54,46 +54,39 @@ def createUser(name):
 
 def encryptMessage(message, name):
     e, n = getpubKeyFromFile(name)
-    #n = getpubKeyFromFile(name)
-    #message = str(message)
     message = int(message)
-    #e = int(e)
-    #n = int(n)
-    k = random.randint(2, (n - 1))
+    k = random.randint(1, (n - 1))
     a = pow(k, e, n)
-    b = (message * pow(a, n, n)) % n
-    return a, b
+    b = (message * pow(a, e, n)) % n
+    return (a, b)
+
+def getMsgFromFile(name):
+    filePath = open("C:/Users/IFES Yoga/Documents/GitHub/Sign-Verify/" + name + ".json", "r")
+    msg = filePath.readline()
+    if msg is not None:
+        a = re.findall('\((.*?),', msg)[0] 
+        b = re.findall('\((.*?),', msg)[0]
+        print(a)
+        print(b)
+        return (int(a), int(b))
 
 def decryptMessage(cipher, name):
     d, n = getprivKeyFromFile(name)
-    #n = getprivKeyFromFile(name)
-    #d = int(d)
-    #n = int(n)
-    #a = encryptMessage(cipher, name)[2]
-    #b = encryptMessage(cipher, name)[3]
-    #a = int(a)
-    #b = int(b)
-    a, b = encryptMessage(cipher, name)
-    plaintxt = (b * pow(a, (n - 1 - d), n)) % n
+    a, b = getMsgFromFile(name)
+    plaintxt = (b * pow(a, d, n)) % n
     return plaintxt
 
 def signMessage(message, name): #Defining function to sign message using ElGamal
     d, n = getprivKeyFromFile(name)
-    #n = getprivKeyFromFile(name)[2]
-    #d = int(d)
-    #n = int(n)
     messageHash = hash(message) #Calculating the hash of the message
-    k = random.randint(1, n - 1) #Generating k such that 1 < k < n
+    k = random.randint(2, n - 1) #Generating k such that 1 < k < n
     r = pow(k, d, n) #Calculating r and s
     s = (messageHash - (d * r)) * (n**(k-1)) % n
-    return r, s #Returning the signature
+    return (r, s) #Returning the signature
 
 def verifySignature(message, signature, name): #Defining function to verify the signed message
     e, n = getpubKeyFromFile(name)
-    #n = getpubKeyFromFile(name)
-    #e = int(e)
-    #n = int(n)
-    signature = signMessage(message, name)
+    #signature = signMessage(message, name)
     messageHash = hash(message) #Calculating the hash of the message
     r, s = signature #Unpacking the signature
     v = pow(s, e, n) * r % n #Calculating v
@@ -111,7 +104,6 @@ def writeMessage(message, sender, receiver):
         f =  open("C:/Users/IFES Yoga/Documents/GitHub/Sign-Verify/" + receiver + ".json", 'w')
         f.write("Message: "+ "\n")
         data = f.write(str(message) + "\n")
-        #data_list = data.split()
         print("Message written in file: " + "C:/Users/IFES Yoga/Documents/GitHub/Sign-Verify/" + receiver + ".json")
         f = open("C:/Users/IFES Yoga/Documents/GitHub/Sign-Verify/" + receiver + ".json", "a")
         print("Receiver: " + receiver)
@@ -120,6 +112,7 @@ def writeMessage(message, sender, receiver):
         f.write(str(signature))
 
 def readMessage(message, receiver, sender):
+    #message = open("C:/Users/IFES Yoga/Documents/GitHub/Sign-Verify/" + receiver + ".json", 'r')
     cipher = decryptMessage(message, sender)
     signature = signMessage(message, sender)
     verify = verifySignature(message, signature, sender)
@@ -131,12 +124,10 @@ def readMessage(message, receiver, sender):
         data = f.read() 
         data_list = data.split() 
     if(message == cipher):
-        print("Message: " + cipher)
+       print("Message: " + cipher)
     else:
         print("Message decryption failed!")
-        #integers = [int(x) for x in data_list if x.isdigit()] # to access the integers
-        #strings = [x for x in data_list if not x.isdigit()] # to access the strings
-    if verifySignature(message, signature, sender):
+    if verify:
         print("Signature: Verified successfully!")
     else:
     	print("Signature: Verification failed!")
@@ -145,22 +136,22 @@ def getpubKeyFromFile(name):
         filePath = open("C:/Users/IFES Yoga/Documents/GitHub/Sign-Verify/keys/" + name + ".pub.json", "r")
         pubKey = filePath.readline()
         if pubKey is not None:
-        #data_list = pubKey.split() 
             #e, n = pubKey.split(", ")
-            e = re.findall('\((.*?),', pubKey)[0]
+            e = re.findall('\((.*?),', pubKey)[0] 
             n = re.findall('\((.*?),', pubKey)[0]
+            #print(e)
+            #print(n)
             return (int(e), int(n))
 
 def getprivKeyFromFile(name):
         filePath = open("C:/Users/IFES Yoga/Documents/GitHub/Sign-Verify/keys/" + name + ".priv.json", "r")
         privKey = filePath.readline()
         if privKey is not None:
-        #data_list = privKey.split() 
             #d, n = privKey.split(", ")
             d = re.findall('\((.*?),', privKey)[0]
             n = re.findall('\((.*?),', privKey)[0]
-            print(d)
-            print(n)
+            #print(d)
+            #print(n)
             return (int(d), int(n))
 
 if(sys.argv[1] == "create-user"):
